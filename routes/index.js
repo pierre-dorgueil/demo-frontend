a94dc905b3778cd021dfb649c0c2012adbd438ec
pierre <pir@test.pir.net>
2020-02-02 18:32:19 +0100

    First Commit

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+var host = process.env.COMPONENT_BACKEND_HOST || '0.0.0.0';
+var port = process.env.COMPONENT_BACKEND_PORT || 8080;
+
+/* GET main page */
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+/* POST main page */
+router.post('/', function (req, res, next) {
+  let url = "http://" + host + ":" + port + "/ticketNumber"; 
+  request.get(url, function( err, response, body) {
+    if(err){
+      res.render('thankyou', { ticketNumber: -1 , ipHost: "none" });
+    } else {
+      let ticketInfo = JSON.parse(body);
+      res.render('thankyou', { ticketNumber: ticketInfo.result, ipHost: ticketInfo.ip });
+    }
+  });
+});
+
+module.exports = router;
