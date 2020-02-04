152a2e3e4a1e4b65a47640ab132795472887e700
pdorguei <pdorguei@redhat.com>
2020-02-04 09:57:27 +0100

    x

--- routes/thankyou.js
+++ routes/thankyou.js
@@ -4,7 +4,7 @@ const router = express.Router();
 /* GET request for thank you page */
 router.get('/', function(req, res, next) {
   console.log('req: ' + req);
-  res.render('thankyou', { ticketNumber: 200 });
+  res.render('thankyou', { ticketNumber: 200, ipHost: '0.0.0.0' });
 });
 
 router.get('')
