93b5e27a9f039e46c84b319f9dc8c1f3835c48e1
Gogs <dorguei@redhat.com>
2020-02-05 18:30:10 +0100

    x

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var thankyouRouter = require('./routes/thankyou');
 
 var app = express();
 
-// view engine setup
+// view engine setup 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
