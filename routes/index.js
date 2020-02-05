07d859ae2faafea45fd8aed69d5c96ef92788a03
pierre <pir@test.pir.net>
2020-02-05 22:41:17 +0100

    x

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var request = require('request');
 var host = process.env.COMPONENT_BACKEND_HOST || '0.0.0.0';
 var port = process.env.COMPONENT_BACKEND_PORT || 8080;
+var myIp = (require('os').networkInterfaces())['eth0'][0]['address'];
 
 /* GET main page */
 router.get('/', function(req, res, next) {
@@ -14,10 +15,10 @@ router.post('/', function (req, res, next) {
   let url = "http://" + host + ":" + port + "/ticket"; 
   request.get(url, function( err, response, body) {
     if(err){
-      res.render('thankyou', { ticketNumber: -1 , ipHost: "none" });
+	res.render('thankyou', { ticketNumber: -1 , ipHost: "none", ipFront: "none" });
     } else {
       let ticketInfo = JSON.parse(body);
-      res.render('thankyou', { ticketNumber: ticketInfo.result, ipHost: ticketInfo.ip });
+      res.render('thankyou', { ticketNumber: ticketInfo.result, ipHost: ticketInfo.ip, ipFront: myIp });
     }
   });
 });
